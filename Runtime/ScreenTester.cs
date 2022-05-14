d24e3e3c19d70c751c1f5318997e151f1aea4d38
MgCohen <44907436+MgCohen@users.noreply.github.com>
2022-05-14 16:45:27 -0300

    Init

---
+++ Runtime/ScreenTester.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scaffold.Screens
+{
+    public class ScreenTester
+    {
+        public static void Hey()
+        {
+            Debug.Log("Hi");
+        }
+    }
+}
